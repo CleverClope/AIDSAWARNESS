ac23eddc0b11554dd0c6ab1ad84779176888ef8e
Pascal <info@softcodes.tech>
2020-02-23 23:35:02 +0300

    worked on login register and hospitals on the map

--- app/src/main/java/com/example/aidsawarness/LoginActivity.java
+++ app/src/main/java/com/example/aidsawarness/LoginActivity.java
@@ -2,33 +2,95 @@ package com.example.aidsawarness;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.aidsawarness.Admin.AdminActivity;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText mEmail, mPassword;
+    Button mLoginBtn;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.emailInput);
+        mPassword = findViewById(R.id.passwordInput);
+        progressBar = findViewById(R.id.progressBar);
+        fAuth = FirebaseAuth.getInstance();
+        mLoginBtn = findViewById(R.id.buttonLoginwithemail);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mPassword.setError("Password Must be More than Characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // authenticate the user
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), UserHome.class));
+                            mEmail.setText("");
+                            mPassword.setText("");
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+
     }
 
-    public void login(View view) {
-        Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
-          startActivity(intent);
-    }
 
     public void loginwithfacebook(View view) {
-        Intent intent = new Intent(LoginActivity.this, UserHome.class);
+        Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
         startActivity(intent);
     }
 
     public void register(View view) {
-        Intent intent= new Intent(LoginActivity.this,SignUp.class);
-        startActivity(intent);
+        Intent x = new Intent(LoginActivity.this, SignUp.class);
+        startActivity(x);
     }
 }
--- app/src/main/java/com/example/aidsawarness/UserHome.java
+++ app/src/main/java/com/example/aidsawarness/UserHome.java
@@ -1,7 +1,14 @@
 package com.example.aidsawarness;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
@@ -9,27 +16,13 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
-
-
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Toolbar;
-
+import com.example.aidsawarness.hospitals.MainActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class UserHome extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    AlertDialog.Builder builder;
 
     private BottomNavigationView bottomNavigationView;
 
@@ -41,17 +34,21 @@ public class UserHome extends AppCompatActivity
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment fragment;
             switch (item.getItemId()) {
-                case R.id.navigationMyProfile:
+                case R.id.nav_hospitals:
+                    Intent intent = new Intent(UserHome.this, FindHospitalsA.class);
+                    startActivity(intent);
                     return true;
-                case R.id.navigationMyCourses:
+                case R.id.nav_drugs:
 
                     return true;
-                case R.id.navigationHome:
+                case R.id.nav_counselor:
+                    Intent c = new Intent(UserHome.this, MainActivity.class);
+                    startActivity(c);
                     return true;
-                case  R.id.navigationSearch:
+                case R.id.nav_tips:
                     return true;
-                case  R.id.navigationMenu:
-                    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                case R.id.nav_settings:
+                    DrawerLayout drawer = findViewById(R.id.drawer_layout);
                     drawer.openDrawer(GravityCompat.START);
                     return true;
             }
@@ -70,14 +67,15 @@ public class UserHome extends AppCompatActivity
         setContentView(R.layout.activity_user_home);
 //     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //     setSupportActionBar(toolbar);
+        builder = new AlertDialog.Builder(this);
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         bottomNavigationView = findViewById(R.id.navigation);
@@ -86,13 +84,13 @@ public class UserHome extends AppCompatActivity
         CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) bottomNavigationView.getLayoutParams();
         layoutParams.setBehavior(new BottomNavigationBehavior());
 
-        bottomNavigationView.setSelectedItemId(R.id.navigationHome);
+        bottomNavigationView.setSelectedItemId(R.id.nav_tips);
 
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -107,17 +105,48 @@ public class UserHome extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.setreminder) {
+            // Handle the reminder
+        } else if (id == R.id.findcounselor) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.healthcenters) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.medicalhistory) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_dark_mode) {
+        } else if (id == R.id.logout) {
+            //Uncomment the below code to Set the message and title from the strings.xml file
+            builder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
+
+            //Setting message manually and performing action on button click
+            builder.setMessage("Do you want to Logout ?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            finish();
+                            Toast.makeText(getApplicationContext(), "Logging you out!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //  Action for 'NO' Button
+                            dialog.cancel();
+                            Toast.makeText(getApplicationContext(), "Enjoy the App",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            //Creating dialog box
+
+            AlertDialog alert = builder.create();
+            //Setting the title manually
+            alert.setTitle("Logout From AIDSAWARNESS");
+            alert.show();
+        } else if (id == R.id.appinfo) {
+
+
+        } else if (id == R.id.editprofile) {
             //code for setting dark mode
             //true for dark mode, false for day mode, currently toggling on each click
             DarkModePrefManager darkModePrefManager = new DarkModePrefManager(this);
@@ -126,7 +155,7 @@ public class UserHome extends AppCompatActivity
             recreate();
 
         }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
